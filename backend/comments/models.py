c80689b4a10e15793edf5209a1f15c46fb47081c
phil-sorenson <phillip.sorenson31@gmail.com>
2023-01-04 21:48:40 -0600

    Created get_comments views.py function to get all comments via video_id + Changed user_id in serializers to just user

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from authentication.models import User
 # Create your models here.
-
+# Text= Body of the comment
+# video_id is a string and how we track specific comments along
+# user will have there own array of different log in info including a user_id
+    # Set up to where you're getting ALL user info upon a POST request
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=30)
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import get_object_or_404
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_comments(request,video_id):
+    comments = Comment.objects.filter(video_id = video_id)
+    serializer= CommentSerializer(comments, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    print(
+        'User', f"{request.user.id} {request.user.email} {request.user.username}"
+    )
